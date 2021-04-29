ec64a8b76c87d59d86cb2b0d1ae214feabe8e541
Martin2696 <mato.siska@gmail.com>
2021-04-29 22:20:28 +0200

    something is happening

--- src/main/java/stockanalyzer/ctrl/Controller.java
+++ src/main/java/stockanalyzer/ctrl/Controller.java
@@ -5,16 +5,46 @@ import yahoofinance.Stock;
 
 import java.io.IOException;
 
-
 public class Controller {
 
+	Stock stock = null;
 
 	public void process(String ticker) throws YahooFinanceException {
 
-		Stock stock = null;
 		try {
-			stock = yahoofinance.YahooFinance.get("AAPL");
-			stock.getHistory().forEach(System.out::println);
+			stock = yahoofinance.YahooFinance.get(ticker);
+
+			var result = stock.getHistory().stream()
+					.mapToDouble(q -> q.getClose().doubleValue())
+					.max()
+					.orElse(0.0);
+
+			stock.getHistory().forEach(q -> System.out.println(q));
+
+			System.out.println();
+			System.out.println(ticker);
+			System.out.println();
+
+			System.out.println("* * * * Max Price * * * * *");
+			System.out.println(result);
+
+			var result2 = stock.getHistory().stream()
+					.mapToDouble(q -> q.getClose().doubleValue())
+					.average()
+					.orElse(0.0);
+
+			System.out.println();
+			System.out.println("* * * * Average Price * * * * *");
+			System.out.println(result2);
+
+			var result3 = stock.getHistory().stream()
+					.mapToDouble(q -> q.getClose().doubleValue())
+					.count();
+
+			System.out.println();
+			System.out.println("* * * * Amount * * * * *");
+			System.out.println(result3);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
